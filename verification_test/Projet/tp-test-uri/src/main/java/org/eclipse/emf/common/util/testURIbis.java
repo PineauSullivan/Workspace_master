4b85198c021d43d1ce7e09d6222315bfbba3d2fd
Sullivan PINEAU <pineau.sullivan@gmail.com>
2016-12-02 14:52:51 +0100

    Master: ajout import dans class test

--- verification_test/Projet/tp-test-uri/src/main/java/org/eclipse/emf/common/util/testURIbis.java
+++ verification_test/Projet/tp-test-uri/src/main/java/org/eclipse/emf/common/util/testURIbis.java
@@ -1,7 +1,15 @@
 package org.eclipse.emf.common.util;
+import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
 /**
  * Created by sullivan on 02/12/16.
  */
 public class testURIbis {
+
+    @Test
+    public void testbla(){
+
+    }
 }
+
