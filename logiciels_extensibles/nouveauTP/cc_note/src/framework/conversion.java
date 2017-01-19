452b901c06d5dd06cdba46ce294573d54c1d9680
Sullivan PINEAU <E124533M@S040V7pc11.ensinfo.sciences.univ-nantes.prive>
2017-01-19 17:52:51 +0100

    Master : correction petit cc logiciel ext

---
+++ logiciels_extensibles/nouveauTP/cc_note/src/framework/conversion.java
@@ -0,0 +1,42 @@
+package framework;
+
+import java.lang.reflect.Method;
+
+public class conversion {
+	public static Object conversonBrutale(Object source, Class<?> targetClasse ){
+		Object obj;
+		try {
+			obj = targetClasse.newInstance();
+			for(Method m : source.getClass().getMethods()){
+				if(isGetter(m)){
+					Method mm;
+					mm = findSetter(m,targetClasse);
+					if(mm!=null)
+						mm.invoke(obj, m.invoke(source));
+				}
+			}
+			return obj;
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return null;
+		
+	}
+	
+	private static boolean isGetter(Method m){
+		String verifType = m.getName().substring(0, 3);
+		String attribut = m.getName().substring(3);
+		return verifType.equals("get")&&!attribut.equals("Class");
+	}
+	
+	private static Method findSetter(Method m, Class<?> cl){
+		String attribut = m.getName().substring(3);
+		try {
+			return cl.getMethod("set"+attribut, m.getReturnType());
+		} catch (Exception e) {
+			System.out.println("perte d'information : "+attribut);
+		}
+		return null;
+	}
+	
+}
