e26bfbc7b71dec8e60a7da49523a77717c26e393
Sullivan PINEAU <pineau.sullivan@gmail.com>
2017-02-28 16:03:40 +0100

    master: avancement fonction goaction

--- Compilation/projet/src/GO/Grammaire.cpp
+++ Compilation/projet/src/GO/Grammaire.cpp
@@ -162,6 +162,33 @@ bool Grammaire::GOAnalyse(Noeud *noeud) {
 }
 
 void Grammaire::GOAction(int act){
+	Noeud *T1;
+	Noeud *T2;
+	switch(act){
+		case 1:
+			
+			break;
+		case 2:
+
+			break;
+		case 3:
+
+			break;
+		case 4:
+
+			break;
+		case 5:
+
+			break;
+		case 6:
+
+			break;
+		case 7:
+
+			break;
+	
+	}
+
 
 }
 
